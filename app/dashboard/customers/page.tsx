3b445ef562fcc8edbd2d32730fadb9864dbae8ba
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-12-16 18:32:07 +0700

    Adding Metadata for SEO

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,11 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Customers',
+};
+
 export default function Customers() {
-  return (
-    <div>Customers</div>
-  )
+    return (
+        <div>Customers</div>
+    )
 }
